c47059b12ab4219216172b3dbbd2b58c10455973
Chef91 <chefgraham1991@gmail.com>
2026-07-16 01:33:31 -0400

    weather update

--- js_weather-app/index.js
+++ js_weather-app/index.js
@@ -1,10 +1,39 @@
 import { weatherKey, weatherUrl } from "./config.js";
 
+// Search Controls
+const searchBox = document.querySelector(".search input")
+const searchBtn = document.querySelector(".search button")
+// Weather Icon
+const weatherIcon = document.querySelector(".weather-icon")
 
+// Right Now - Weather Update
 async function checkWeather(city) {
   const response = await fetch(weatherUrl + city + "&appid=" + weatherKey);
   const data = await response.json();
-  console.log(data) 
+  document.querySelector(".city").innerHTML = data.name;
+  document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+  document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+  document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
+  // Image Change by weather
+  if(data.weather[0].main === "Clouds"){
+    weatherIcon.src = "/images/cloud.png"
+  }else if(data.weather[0].main === "Rain"){
+    weatherIcon.src = "/images/rain.png"
+  }else if(data.weather[0].main === "Rain"){
+    weatherIcon.src = "/images/rain.png"
+  }else if(data.weather[0].main === "Rain"){
+    weatherIcon.src = "/images/rain.png"
+  }else if(data.weather[0].main === "Rain"){
+    weatherIcon.src = "/images/rain.png"
+  }else if(data.weather[0].main === "Rain"){
+    weatherIcon.src = "/images/rain.png"
+  }else{
+    weatherIcon.src = "/images/rain.png"
+  }
 }
 
-checkWeather("Orlando");
+// Search Location Using checkWeather Function
+searchBtn.addEventListener("click", ()=>{
+  checkWeather(searchBox.value);
+})
+
